647373792c77551ae4a3ab9f8fe9870f2d9f0052
benedictvimal2 <benedictvimal2@gmail.com>
2024-05-28 20:18:25 +0530

    completed question 3

---
+++ js/question3.js
@@ -0,0 +1,46 @@
+class Person{
+    constructor(firstname,lastname,Dob,location)
+    {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.Dob = Dob;
+        this.location = location;
+    }
+}
+var person1 = new Person("Vimal", "Raj", "17-07-1991" , "Coimbatore");
+var person2 = new Person("Arun", "Kumar", "06-03-1994" , "Bangalore");
+var person3 = new Person("Catherine", "Sandya", "12-10-1998" , "Coimbatore");
+var person4 = new Person("Queen", "Nancy", "19-10-1996" , "Chennai");
+var person5 = new Person("Leander", "Rex", "5-7-1998" , "Kuwait");
+
+console.log(person1.firstname);
+console.log(person1.lastname);
+console.log(person1.Dob);
+console.log(person1.location);
+
+console.log(person2.firstname);
+console.log(person2.lastname);
+console.log(person2.Dob);
+console.log(person2.location);
+
+console.log(person3.firstname);
+console.log(person3.lastname);
+console.log(person3.Dob);
+console.log(person3.location);
+
+console.log(person4.firstname);
+console.log(person4.lastname);
+console.log(person4.Dob);
+console.log(person4.location);
+
+console.log(person5.firstname);
+console.log(person5.lastname);
+console.log(person5.Dob);
+console.log(person5.location);
+
+
+console.log(`All the Person Names are "person1" : ${person1.firstname}
+"person2":${person2.firstname}
+"person3":${person3.firstname}
+"person4":${person4.firstname}
+"person5":${person5.firstname}`)
